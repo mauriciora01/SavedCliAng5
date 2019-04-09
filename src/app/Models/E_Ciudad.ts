ad78e14d0da75a025f33eeb09b174a99f0d653b9
unknown <ramosgimauricio@gmail.com>
2019-04-09 00:23:25 -0500

    se agregaron modelos de ciudad info

---
+++ src/app/Models/E_Ciudad.ts
@@ -0,0 +1,23 @@
+
+import { E_Error } from "./E_Error";
+import { E_ZonaxCiudad } from "./E_ZonaxCiudad";
+
+export class E_Ciudad {
+
+    public CodCiudad: string
+    public NombreCiudad: string
+    public CodEstado: string
+    public ReteIca: string
+    public CodigoCiudad: string
+    public CodigoServientrega: string
+    public ValorFlete: number
+    public ExcluidoIVA: number
+    public IVA: number
+    public PedidoMin: number
+    public ValorFleteFull: number
+    public ZonaxCiudad: E_ZonaxCiudad
+    public error: E_Error
+    
+    constructor() { }
+}
+
---
+++ src/app/Models/E_ZonaxCiudad.ts
@@ -0,0 +1,17 @@
+
+import { E_Error } from "./E_Error";
+
+export class E_ZonaxCiudad {
+
+    public CodigoCiudad: number
+    public Zona: string
+    public NombreCiudad: string
+    public CodigoCiudadString: string
+    public Sector: string
+    public Usuario: string
+    public ZonasAsignadaxCiudad: string
+    public error: E_Error
+    
+    constructor() { }
+}
+
